13a6a83052686f4fde83cff9262cd6b667413565
TOFAYEL H TOPU <tofayelhtopu@gmail.com>
2023-12-24 23:04:50 +0600

    ..

--- Basic/dataType.js
+++ Basic/dataType.js
@@ -1,21 +1,21 @@
 /**
- * JavaScript is a dynamically typed language, meaning that a variable isn't associated with a specific type.
-   In other word, a variable can hold a value of different type.
+    JavaScript is a dynamically typed language, meaning that a variable isn't associated with a specific type.
+    In other word, a variable can hold a value of different type.
 
-   -----------JavaScript Data Types ------------
- * JavaScript has 7 primitive data type.
-    1. null
-    2. undefine
-    3. boolean
-    4. string
-    5. number
-    6. symbol -ES2015
-    7. bigint -ES2020
- * JavaScript complex(reference to its value) data type
-    1. object
-    2. array
-    3. function
- */
+    JavaScript has 7 primitive data type.
+     1. null
+     2. undefine
+     3. boolean
+     4. string
+     5. number
+     6. symbol -ES2015
+     7. bigint -ES2020
+
+    JavaScript complex(reference to its value) data type
+     1. object
+     2. array
+     3. function
+ **/
 
     // The null type
     let obj = null;
