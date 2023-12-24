36641a0a03fa470e86526879f18d17f5d683db4a
TOFAYEL H TOPU <tofayelhtopu@gmail.com>
2023-12-25 00:09:20 +0600

    data types in js

--- Basic/dataType.js
+++ Basic/dataType.js
@@ -15,27 +15,55 @@
      1. object
      2. array
      3. function
- **/
+ */
 
-    /*
-    * The null type.
-    * The type of obj returns object is a known bug in js.
-    **/
+    /**
+     * The null type.
+     * 
+     * The type of obj returns object is a known bug in js.
+     */
     let obj = null;
     console.log('null type: ', obj, typeof obj); // null object
 
-    // The undefined type
+    /**
+     * The undefined type.
+     * 
+     * When a variable is declared but not initialized,
+       its default value is undefined.
+     */
     let firstName;
     let lastName = undefined;
     console.log('undefine type: ', firstName, lastName, typeof firstName); // undefined undefined undefined
-    /* When a variable is declared but not initialized, its default value is undefined. */
 
-    // The boolean type
+    /**
+     * The boolean type.
+     * 
+     * Using Boolean() function, to convert values of other types into boolean value.
+     */
     let inProgress = true;
     let done = false;
     console.log('boolean type: ', inProgress, done, typeof inProgress); // true false boolean
-    /* Using Boolean() function, to convert values of other types into boolean value */
+
     console.log('string Boolean: ', Boolean('Hello'), Boolean('')); // true false
     console.log('number Boolean: ', Boolean(10), Boolean(Infinity), Boolean(0), Boolean(NaN)); // true true false false
     console.log('object Boolean: ', Boolean({key: 10}), Boolean(null)); // true false
     console.log('undefine Boolean: ', Boolean(undefined)); // false
+    
+    /**
+     * The string type.
+     * 
+     * string literal can be ' ' or " "
+     */
+    let message = 'Hello';
+    let str = "Hi";
+    console.log('string type: ', message, str, typeof message); // Hello Hi string
+
+    /**
+     * The number type.
+     * 
+     * use number type to represent both integer and floating-point numbers.
+     */
+    let price = 100;
+    let discount = 0.75;
+    let total = 99.25;
+    console.log('number type: ', price, discount, total,  typeof price, typeof total); // 100 0.75 99.25 number number
