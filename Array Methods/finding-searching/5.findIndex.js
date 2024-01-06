91c3bec13350be2d1efa001ce2879a1175f7d768
SWOSTI-TOPU <tofayelhtopu@gmail.com>
2024-01-06 11:12:50 +0600

    array method findIndex in details

--- Array Methods/finding-searching/5.findIndex.js
+++ Array Methods/finding-searching/5.findIndex.js
@@ -1,7 +1,7 @@
 /**
  * Array.prototype.findIndex() method.
- * The findIndex() method returns the first element in an array that passes a test function.
- * If no values satisfy the testing function, undefined is returned.
+ * The findIndex() method returns the index of the first element in an array that satisfies the provided testing function.
+ * If no elements satisfy the testing function, -1 is returned.
  * The findIndex() method is an iterative method. It calls a provided callbackFn function once for each element 
    in an array in ascending-index order, until callbackFn returns a truthy value. 
  * 
@@ -9,26 +9,24 @@
  * findIndex(callbackFn, thisArg)
  * 
  * Parameters: callbackFn(element , index, array), thisArg(optional).
- * Return: The first element in the array that satisfies the provided testing function. Otherwise, undefined is returned.
+ * Return: The index of the first element in the array that passes the test. Otherwise, -1.
  */
 
-   // find an object by id
-   const students = [
-    {id: 1, name: 'Jasim', age: 15},
-    {id: 2, name: 'Selim', age: 20},
-    {id: 3, name: 'Tanvir', age: 15},
-    {id: 4, name: 'banalata sen', age: 19},
-    {id: 5, name: 'Kabir', age: 13}
-];
-function getStudentById(element, index, array) {
-    // console.log('student: ', element, index, array);
-    return element.id = 4
-}
-const student = students.find(getStudentById)
-console.log('student: ', student);
-
-// Using arrow function and destructuring.
-const result1 = students.find(item => item.id === 3);
-const result2 = students.find(({id}) => id === 5);
-console.log('result: ', result1, result2); //  { id: 3, name: 'Tanvir', age: 15 } { id: 5, name: 'Kabir', age: 13 }
+    // find index in simple array.
+    const numbers = [10, 20, 30, 40, 20, 60, 30, 80];
+    const result1 = numbers.findIndex((number, index) => number === 30); 
+    const result2 = numbers.findIndex((number, index) => (number === 30 && index > 2)); 
+    console.log('result: ', result1, result2); // 2 6
+    
+    // find index in array of objects using arrow function and destructuring.
+    const students = [
+      {id: 1, name: 'Jasim', age: 15},
+      {id: 2, name: 'Selim', age: 20},
+      {id: 3, name: 'Tanvir', age: 15},
+      {id: 4, name: 'banalata sen', age: 19},
+      {id: 5, name: 'Kabir', age: 13}
+    ];
+    const student1 = students.findIndex(item => item.id === 3);
+    const student2 = students.findIndex(({id}) => id === 5);
+    console.log('result: ', student1, student2); //  2 4
 
